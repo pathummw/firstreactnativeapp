a5c6adbb605bd90944c4aa20997567ab351365fc
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-08-25 01:05:20 +0200

    Display the user input

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [input, setInput] = useState(0);
+
+  const clickHandler = () => {
+    setInput(input + 2);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello {input}</Text>
+
+      <View style={styles.buttonContainer}>
+        <TextInput style={styles.input}
+          placeholder='e.g. Pathum'
+          onChangeText={(e) => setInput(e)}
+          keyboardType='numeric' />
+        <Button title='Enter' onPress={clickHandler} />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +34,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  buttonContainer: {
+    marginTop: 20
+  },
+  input: {
+    borderBottomColor: 'black',
+    padding: 8,
+    margin: 10,
+    borderWidth: 1,
+  }
 });
