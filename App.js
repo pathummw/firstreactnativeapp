a078d836afba4b604c8a5a6880f0ab9f6e334203
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-08-25 01:43:19 +0200

    Calc layout

--- App.js
+++ App.js
@@ -4,25 +4,27 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
-  const [input, setInput] = useState(0);
+  /* const [input, setInput] = useState(0);
 
   const clickHandler = () => {
     setInput(input + 2);
-  }
+  } */
 
   return (
     <View style={styles.container}>
-      <Text>Hello {input}</Text>
-
-      <View style={styles.buttonContainer}>
+      {/* <View style={styles.buttonContainer}>
         <TextInput style={styles.input}
           placeholder='e.g. Pathum'
           onChangeText={(e) => setInput(e)}
           keyboardType='numeric' />
         <Button title='Enter' onPress={clickHandler} />
+      </View> */}
+      <View style={styles.display}></View>
+      <View style={styles.buttons}>
+        <View style={styles.numbers}></View>
+        <View style={styles.operations}></View>
       </View>
 
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -30,18 +32,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    /* backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    zIndex: -10, */
   },
 
-  buttonContainer: {
-    marginTop: 20
+  display: {
+    backgroundColor: 'yellow',
+    flex: 2,
   },
-  input: {
-    borderBottomColor: 'black',
-    padding: 8,
-    margin: 10,
-    borderWidth: 1,
-  }
+  buttons: {
+    /* backgroundColor: 'green', */
+    flex: 6,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: 'grey',
+
+
+  },
+  operations: {
+    flex: 1,
+    backgroundColor: 'green',
+    flexDirection: 'column'
+  },
 });
